ffa926b2a046fbc712407ee76dc3467b8206446b
Joe <joe.bell.1329@gmail.com>
2019-09-30 16:40:07 +0100

    allow vault modification

--- src/api/vault.ts
+++ src/api/vault.ts
@@ -2,7 +2,6 @@ import { singleton } from 'tsyringe';
 import { JAuthServer } from '../server';
 import { JAuthDb } from '../db';
 import { Next, Request, Response } from 'restify';
-import { ObjectId } from 'mongodb';
 
 @singleton()
 export class VaultController {
@@ -12,12 +11,12 @@ export class VaultController {
   constructor(private jauthServer: JAuthServer, private jauthDb: JAuthDb) { }
 
   public register() {
-    this.jauthServer._server.post(`${this.baseUrl}`, (req, res, next) => this.createVault(req, res, next));
     this.jauthServer._server.get(`${this.baseUrl}`, (req, res, next) => this.getVaultForUser(req, res, next));
+    this.jauthServer._server.post(`${this.baseUrl}`, (req, res, next) => this.createVault(req, res, next));
+    this.jauthServer._server.put(`${this.baseUrl}`, (req, res, next) => this.updateVault(req, res, next));
   }
 
   private createVault(req: Request, res: Response, next: Next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
     const vaultCollection = this.jauthDb._db.collection('vaults');
     vaultCollection.insertOne(req.body, (err, result) => {
       if (err) {
@@ -29,9 +28,19 @@ export class VaultController {
   }
 
   private getVaultForUser(req: Request, res: Response, next: Next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
     const vaultCollection = this.jauthDb._db.collection('vaults');
     vaultCollection.findOne({ email: req.query.email }, (err, result) => {
+      if (err) {
+        res.status(500);
+        res.send();
+      }
+      res.send(result);
+    });
+  }
+
+  private updateVault(req: Request, res: Response, next: Next) {
+    const vaultCollection = this.jauthDb._db.collection('vaults');
+    vaultCollection.findOneAndReplace({ email: req.body.email }, req.body, (err, result) => {
       if (err) {
         res.status(500);
         res.send();
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,23 @@
 import { createServer, plugins, Server } from 'restify';
 import { singleton } from 'tsyringe';
-import jsonBodyParser = plugins.jsonBodyParser;
-import queryParser = plugins.queryParser;
+import * as corsMiddleware from 'restify-cors-middleware';
 
 @singleton()
 export class JAuthServer {
 
   _server: Server;
+  cors = corsMiddleware({
+    origins: ['*'],
+    allowHeaders: ['Authorization'],
+    exposeHeaders: ['Authorization']
+  });
 
   constructor() {
     this._server = createServer();
-    this._server.use(jsonBodyParser());
-    this._server.use(queryParser());
+    this._server.use(plugins.jsonBodyParser());
+    this._server.use(plugins.queryParser());
+    this._server.pre(this.cors.preflight);
+    this._server.use(this.cors.actual);
   }
 
   public start() {
@@ -19,5 +25,4 @@ export class JAuthServer {
       console.log('Server listening on port 8000');
     });
   }
-
 }
